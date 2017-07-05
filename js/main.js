1e350cfb0cb44a86630e0049535a989aa48acc05
Karina <ymo4ka.k@gmail.com>
2017-07-05 10:50:36 +0300

    SPWEB Home Page Mobile 0507.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-﻿
+﻿$( document ).ready(function() {
+	$(".navbar-toggle").click(function () {
+		$(".header-nav").toggleClass("opened-menu");
+		//setTimeout(function () {
+			if ($(".header-nav").hasClass("opened-menu")) {
+				$(".navbar-toggle").addClass("fixed");
+				$("body").addClass("fixed");
+			} else {
+				$(".navbar-toggle").removeClass("fixed");
+				$("body").removeClass("fixed");
+			}	
+		//}, 0);
+	});
+});
