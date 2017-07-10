350a277f26c974c1f1188cf843c79e8f4036aff6
Karina <ymo4ka.k@gmail.com>
2017-07-10 17:42:04 +0300

    SPWEB Home Page Animations (desktop and mobile).

--- js/main.js
+++ js/main.js
@@ -1,30 +1,32 @@
 ﻿$( document ).ready(function() {
+	var maxHeaderHeight = $("header").css("max-height");
+	var minHeaderHeight = $("header").css("min-height");
 	$(".navbar-toggle").click(function () {
 		$(".header-nav").toggleClass("opened-menu");
 		//setTimeout(function () {
 			if ($(".header-nav").hasClass("opened-menu")) {
 				$(".navbar-toggle").addClass("fixed");
 				$("body").addClass("mobile-fixed");
-				$("header").css("z-index", "4");
+				//$("header").css("z-index", "4");
 			} else {
 				$(".navbar-toggle").removeClass("fixed");
 				$("body").removeClass("mobile-fixed");
-				$("header").css("z-index", "0");
+				//$("header").css("z-index", "0");
 			}	
 		//}, 0);
 	});
 
-	$(window).scroll(function () {
-		if (document.documentElement.clientWidth < 768) {
-	        if ($(document).scrollTop() > 10) {
-	            $("header").css("height", "61px");
-	            $("header").css("z-index", "4");
-	            $(".banner-text").css("display", "none");
-	        } else {
-	            $("header").css("height", "200px");
-	            $("header").css("z-index", "0");
-	            $(".banner-text").css("display", "block");
-	        }
-    	}
-    });
+	 $(window).scroll(function () {
+	 	//if (document.documentElement.clientWidth < 768) {
+	         if ($(document).scrollTop() > 10) {
+	             $("header").css("height", minHeaderHeight);
+	             //$("header").css("z-index", "4");
+	             $(".banner-text").css("display", "none");
+	         } else {
+	             $("header").css("height", maxHeaderHeight);
+	             //$("header").css("z-index", "0");
+	             $(".banner-text").css("display", "block");
+	         }
+     	//}
+     });
 });
