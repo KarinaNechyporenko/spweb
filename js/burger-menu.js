22068b3878bc869bfdab7935b1b06f3f6160b83e
Karina <ymo4ka.k@gmail.com>
2017-07-13 15:19:23 +0300

    SPWEB Single Product Page 1307-1.

---
+++ js/burger-menu.js
@@ -0,0 +1,16 @@
+﻿$( document ).ready(function() {
+	$(".navbar-toggle").click(function () {
+		$(".header-nav").toggleClass("opened-menu");
+		//setTimeout(function () {
+			if ($(".header-nav").hasClass("opened-menu")) {
+				//$(".navbar-toggle").addClass("fixed");
+				$("body").addClass("mobile-fixed");
+				//$("header").css("z-index", "4");
+			} else {
+				//$(".navbar-toggle").removeClass("fixed");
+				$("body").removeClass("mobile-fixed");
+				//$("header").css("z-index", "0");
+			}	
+		//}, 200);
+	});
+});
--- js/main.js
+++ js/main.js
@@ -3,20 +3,6 @@
 	var minHeaderHeight = $("header").css("min-height");
 	var maxContentTop;
 	var minContentTop; 
-	$(".navbar-toggle").click(function () {
-		$(".header-nav").toggleClass("opened-menu");
-		//setTimeout(function () {
-			if ($(".header-nav").hasClass("opened-menu")) {
-				//$(".navbar-toggle").addClass("fixed");
-				$("body").addClass("mobile-fixed");
-				//$("header").css("z-index", "4");
-			} else {
-				//$(".navbar-toggle").removeClass("fixed");
-				$("body").removeClass("mobile-fixed");
-				//$("header").css("z-index", "0");
-			}	
-		//}, 200);
-	});
 
 	 $(window).scroll(function () {
 	 	minContentTop = parseInt($("header").css("min-height")) + 160;
