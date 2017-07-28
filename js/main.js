a4859c7884f4d057c9436494180766f1f62df447
Karina <ymo4ka.k@gmail.com>
2017-07-28 16:05:54 +0300

    SPWEB smooth scroll 2807-1.

--- js/main.js
+++ js/main.js
@@ -5,6 +5,16 @@
 	var minContentTop;
     var pageTop;
 
+    //minContentTop = parseInt($("header").css("min-height")) + 160;
+    minContentTop = parseInt($(".page-content").css("top")) - 160;
+        if (document.documentElement.clientWidth < 768) {
+            maxContentTop = parseInt($("header").css("max-height")) + 60;           
+        }
+        else {
+            maxContentTop = parseInt($("header").css("max-height"));
+            //maxContentTop = parseInt($(".page-content").css("top"));
+        }
+
 	/**** Header Resize without smooth scroll Start ****/
 
     /*$(window).scroll(function () {
@@ -47,41 +57,58 @@
             updateOnContentResize:true,
             theme:"dark-thin",
             mouseWheelPixels:600,
+            scrollInertia:600,
             callbacks:{
+                onScrollStart:function(){
+                    headerResizeMin(this);
+                },
                 onScroll:function(){
-                    headerResize(this);
+                    headerResizeMax(this);
                 }
             }
 
         });
     });
         
-    function headerResize(el){
-        minContentTop = parseInt($("header").css("min-height")) + 160;
-        if (document.documentElement.clientWidth < 768) {
-            maxContentTop = parseInt($("header").css("max-height")) + 60;           
-        }
-        else {
-            maxContentTop = parseInt($("header").css("max-height"));
-        }
+    function headerResizeMin(el){
+        //$(".mCSB_container").css("top", "-100px");
+        //console.log($(".mCSB_container").css("top"));
         pageTop = parseInt($(".mCSB_container").css("top"));
         //if ($(".mCSB_container").scrollTop() < -10) {
-        if (pageTop < -10) {
-            console.log("yep");
+        if (pageTop == 0) {
+            // console.log("yep");
             $(".banner-text").css("display", "none");
             $("header").css("height", minHeaderHeight);
-            $(".page-content").css("top", minContentTop + "px");
+                //$(".page-content").css("top", minContentTop + "px");
             if (document.documentElement.clientWidth > 768) {
                 $("header").css("z-index", "4");
             }
+            $('body').mCustomScrollbar('scrollTo',minContentTop);
              
-        } else {
+        } 
+        // else if (pageTop > -200) {
+        //     $("header").css("height", maxHeaderHeight);
+        //     $(".banner-text").css("display", "block");
+        //     $(".page-content").css("top", maxContentTop + "px");
+        //     if (document.documentElement.clientWidth > 768) {
+        //         $("header").css("z-index", "0");
+        //     }
+        //     $('body').mCustomScrollbar('scrollTo', '0px');
+             
+        // }
+    };
+     function headerResizeMax(el){
+        
+        pageTop = parseInt($(".mCSB_container").css("top"));
+        //if ($(".mCSB_container").scrollTop() < -10) {
+        if (pageTop > -200) {
             $("header").css("height", maxHeaderHeight);
             $(".banner-text").css("display", "block");
             $(".page-content").css("top", maxContentTop + "px");
             if (document.documentElement.clientWidth > 768) {
                 $("header").css("z-index", "0");
             }
+            //$('body').mCustomScrollbar('scrollTo', '0px');
              
         }
     };
